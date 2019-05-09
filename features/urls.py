0a90d7ac6d16883a4efb9e481350bd30903d9703
Des Flaherty <desflaherty@hotmail.com>
2019-05-09 09:43:02 +0000

    adding fucntionality to add feature through front end button

--- features/urls.py
+++ features/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url
-from .views import all_features
+from django.conf.urls import url,include
+from .views import all_features,add_edit_feature
 
 urlpatterns = [
     url(r'^$', all_features, name='features'),
+    url(r'^edit_feature/(?P<id>\d+)/$', add_edit_feature, name='edit_feature'),
+    url(r'^add_feature/$', add_edit_feature, name='add_feature'),
 ]
--- features/views.py
+++ features/views.py
@@ -1,7 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render,reverse,redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Features
-# Create your views here.
+from django.contrib.auth.models import User
+from .forms import FeaturesForm
 
 def all_features(request):
     features = Features.objects.all()
     return render(request, 'features.html', {'features': features})
+
+
+@login_required
+def add_edit_feature(request, id=None):
+    """Renders the add or edit page and saves posted tickets  """
+
+    features = get_object_or_404(Features, id=id) if id else None
+    user = str(request.user)
+    add_edit = True
+    if features == None:
+        add_edit = False
+    if request.method == "POST":
+        form = FeaturesForm(request.POST, request.FILES, instance=features)
+        
+        if form.is_valid():
+            form = form.save(commit=False)
+        if user == 'admin' or user == 'testadmin':
+                form.status = request.POST.get('status')    
+            
+        if features == None:
+                form.username = request.user
+                form.save()
+                return redirect(reverse(all_features))
+                
+        else:
+                form.username = features.username 
+                form.save()
+                return redirect(features, id)
+      
+    else:
+        form = FeaturesForm(instance=features)
+    return render(request, 'add_feature.html', {'add_edit': add_edit,
+                                               'form': form})
