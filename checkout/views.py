97c2726b1568b4712734d1cbe47c9eea138d5ec1
Des Flaherty <desflaherty@hotmail.com>
2019-05-11 13:11:09 +0000

    adding views to checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import MakePaymentForm
+from django.conf import settings
+from django.utils import timezone
+from features.models import Features
+import stripe
 
-# Create your views here.
+stripe.api_key = settings.STRIPE_SECRET
+
+
+@login_required()
+def checkout(request):
+    """Renders the checkout page. If post, payment is taken or 
+       error is returned  """
+    if request.method == "POST":
+
+        payment_form = MakePaymentForm(request.POST)
+
+        if payment_form.is_valid():
+            cart = request.session.get('cart', {})
+            total = 0
+
+            for id, quantity in cart.items():
+                feature = get_object_or_404(Features, pk=id)
+                total += quantity * 100
+            try:
+                customer = stripe.Charge.create(
+                    amount=int(total * 100),
+                    currency="EUR",
+                    description=request.user.email,
+                    card=payment_form.cleaned_data['stripe_id'],
+                )
+            except stripe.error.CardError:
+                messages.error(request, "Your card was declined!")
+
+            if customer.paid:
+                messages.success(request, "You have successfully paid")
+                return redirect(reverse('upvote_feature'))
+            else:
+                messages.error(request, "Unable to take payment")
+        else:
+            messages.error(
+                request, "We were unable to take a payment with that card!")
+    else:
+        payment_form = MakePaymentForm()
+
+    return render(request, "checkout.html", {'payment_form': payment_form,
+                                             'publishable': settings.STRIPE_PUBLISHABLE})
