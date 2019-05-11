26e442163a43c14994cd56c39247f6402a14f31c
Des Flaherty <desflaherty@hotmail.com>
2019-05-11 13:59:49 +0000

    adding checkout html page

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from .views import checkout
 
 urlpatterns = [
-    url(r'^$', checkout, name="checkout"),
+    url(r'^/$', checkout, name="checkout"),
 ]
+
--- unicorntracker/urls.py
+++ unicorntracker/urls.py
@@ -23,6 +23,7 @@ from search import urls as urls_search
 from django.views import static
 from .settings import MEDIA_ROOT
 from home.views import index
+from checkout import urls as urls_checkout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -30,6 +31,7 @@ urlpatterns = [
     url(r'^accounts/', include(urls_accounts)),
     url(r'^features/', include(urls_features)),
     url(r'^cart/', include(urls_cart)),
+    url(r'^checkout/', include(urls_checkout)),
     url(r'^search/', include(urls_search)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
