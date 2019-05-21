9224d81aa7e65d55ab9924446e8fa20d1b878ee1
Des Flaherty <desflaherty@hotmail.com>
2019-05-21 12:19:30 +0000

    creating bugs model fields

--- bugs/models.py
+++ bugs/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Features(models.Model):
+    ToDo = 'To Do'
+    Doing = 'Doing'
+    Done = 'Done'
+    STATUS_CHOICES = ((ToDo, 'To Do'),
+                      (Doing, 'Doing'), (Done, 'Done'))
+
+    name = models.CharField(max_length=50, blank=False)
+    description = models.TextField(blank=False)
+    username = models.ForeignKey(User, default=None)
+    created_date = models.DateTimeField(blank=True, default=None, null=True)
+    waiting_date = models.DateTimeField(blank=True, default=None, null=True)
+    in_progress_date = models.DateTimeField(
+        blank=True, default=None, null=True)
+    completion_date = models.DateTimeField(blank=True, default=None, null=True)
+    views = models.IntegerField(default=0)
+    upvotes = models.IntegerField(default=0)
+    status = models.CharField(
+        max_length=40, choices=STATUS_CHOICES, default=ToDo)
+      
+
+    def __str__(self):
+        return self.name
