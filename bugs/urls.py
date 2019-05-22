4e957b571c8f641b86615e324be9af7389865088
Des Flaherty <desflaherty@hotmail.com>
2019-05-22 10:50:51 +0000

    fixing error in upvote bug url

--- bugs/urls.py
+++ bugs/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^bug_detail/(?P<id>\d+)/$', bug_detail, name='bug_detail'),
     url(r'^edit_bug/(?P<id>\d+)/$', add_edit_bug, name='edit_bug'),
     url(r'^add_bug/$', add_edit_bug, name='add_bug'),
-    url(r'^upvote_bug/$', upvote_bug, name='upvote_bug'),
+    url(r'^upvote_bug/(?P<id>\d+)/$', upvote_bug, name='upvote_bug'),
     url(r'^add_comment_bugs/(?P<id>\d+)/$',
         add_comment_bugs, name='add_comment_bugs'),
 ]
