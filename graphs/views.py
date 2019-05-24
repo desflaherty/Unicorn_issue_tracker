18e71d15fe45ae4d032dbcc8cc6041c6ba49af90
Des Flaherty <desflaherty@hotmail.com>
2019-05-24 16:15:52 +0000

    adding features-graphs.html

--- graphs/views.py
+++ graphs/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from graphs import FeaturesPieChart, BugsPieChart
+from graphs.graphs import FeaturesPieChart, BugsPieChart
 
 def graphs(request):
     """ Charts """
     return render(request, 'features-graphs.html',
-                  {'feature_chart': FeaturesPieChart(),
-                   'bugs_chart': BugsPieChart()
+                  {'FeaturesPieChart': FeaturesPieChart(),
+                   'BugsPieChart': BugsPieChart()
                    })
